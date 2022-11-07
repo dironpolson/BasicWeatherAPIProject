dd8cd7ee99533ea28d07b8a448b4d8a57bb79213
DironPolson <dironpolson404@gmail.com>
2022-11-07 21:14:52 +0100

    First commit of Weather API

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.static("src"));
@@ -51,5 +51,5 @@ app.post("/", (req, res) => {
 
 
 app.listen(port, () => {
-    console.log("Server is running on port 3000!")
+    // console.log("Server is running on port 3000!")
 });
